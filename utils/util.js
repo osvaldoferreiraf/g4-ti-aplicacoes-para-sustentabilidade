612dfa80068637a4b502386451f824f4d06c1173
pedroluckeroth <pedroluckeroth@gmail.com>
2024-11-17 13:52:55 -0300

    util

---
+++ utils/util.js
@@ -0,0 +1,86 @@
+import atendimentoVeterinario from "../public/assets/img/atendimentoVeterinario.svg";
+import castracao from "../public/assets/img/castracao.svg";
+import limpezaTartaro from "../public/assets/img/limpezaTartaro.svg";
+import cirurgias from "../public/assets/img/cirurgias.svg";
+import taxiDog from "../public/assets/img/taxiDog.svg";
+import vacinacao from "../public/assets/img/vacinacao.svg";
+
+export const servicos = [
+  {
+    id: 1,
+    title: "ATENDIMENTO VETERINÁRIO",
+    img: atendimentoVeterinario,
+    description: `O direito a uma boa saúde não é exclusividade do ser humano, os animais também precisam de todo o cuidado e alimentação, passando por atendimento veterinário de qualidade, portanto os animais necessitam e merecem uma atenção especial, afinal alguns são considerados os melhores amigos do homem.<br>`,
+    contatoText: `Para mais informações sobre esse serviço, entre em contato conosco pelo telefone: (31) 4102-0020 ou (31) 9 8334-7176`,
+  },
+  {
+    id: 2,
+    title: "CASTRAÇÃO",
+    img: castracao,
+    description: `A castração de cachorro envolve uma série de mitos e crenças mas, nos dias de hoje, esse procedimento é altamente recomendado pelos profissionais veterinários, já que traz muitos benefícios para os cães – e também para os seus donos. Impedindo a procriação, o processo é tido como um grande aliado para a diminuição do nível de abandono de animais. No entanto, as suas vantagens vão muito além disso.<br><br>
+        Ajudando a controlar e até evitar, por completo, diversos comportamentos incômodos e até doenças que podem surgir no animal, a castração de cachorro é indicada quando o cão atinge a sua maturidade sexual – o que ocorre por volta de um ano de vida. Cachorros mais velhos também podem ser castrados sem complicações, no entanto, benefícios como a diminuição da agressividade podem não ser notados quando o cão que passa pelo procedimento já tem uma idade mais avançada.<br><br>
+        Embora haja uma série de mitos que envolvem a castração de cães – incluindo a ideia de que o animal pode sofrer por não ter uma vida sexual ou que ele pode adquirir uma propensão maior a desenvolver doenças – a grande maioria deles são falsos, e o procedimento, na realidade, traz muito mais vantagens que desvantagens para o animal.<br><br>
+        Confira, neste artigo, como é realizado o procedimento e quais são as precauções que devem ser tomadas para que ele ocorra sem problemas; e descubra quais são as principais vantagens que a castração de cachorros pode trazer para você e o seu pet.<br><br>
+        O procedimento da castração em cachorros<br>
+        Assim como em qualquer outro tipo de cirurgia em animais, uma consulta com um médico veterinário é fundamental para evitar problemas no processo de castração. Para garantir que tudo irá ocorrer da maneira mais tranquila possível, muitos profissionais do mundo animal realizam uma bateria de exames antes do procedimento, certificando-se de que a saúde do animal está em dia para uma cirurgia.<br><br>
+        Infecções, anemias, coagulação, e doenças diversas (incluindo complicações nos rins e no fígado) além da saúde do coração, fazem parte dos fatores a serem observados antes da castração; sendo que, em alguns casos – como nos que o cão a ser operado possui problemas no coração – ainda mais testes podem ser requeridos. Estes exames pré-cirúrgicos vão variar de necessidade de acordo com a idade e condição geral do animal.<br><br>
+        Consistindo na retirada dos testículos do macho – evitando que ele produza espermatozóides e testosterona e, consequentemente, que possa reproduzir – o processo de castração em cachorros recomenda que, no dia da cirurgia, o animal permaneça em jejum. Devidamente anestesiado, o cão tem a região a ser operada raspada e limpa, e um pequeno corte é feito pouco acima do seu escroto – possibilitando que o cirurgião veterinário faça a retirada dos testículos do animal.<br><br>
+        Bastante rápida e simples, a cirurgia de castração permite que o animal já recupere a maior parte de suas atividades normais em cerca de 24 horas – no entanto, alguns cuidados específicos devem ser tomados durante o período pós-operatório, como:<br>
+        - Prestar atenção nos pontos da operação para verificar possíveis inchaços;<br>
+        - Uso de um colar elizabethano para impedir que o cão morda ou lamba a área da incisão;<br>
+        - Administração de medicamentos indicados pelo médico veterinário;<br>
+        - Restrição de atividades do cão como pular ou correr por cerca de dez dias após a cirurgia.<br><br>
+        Benefícios da castração em cachorros<br>
+        Conforme dito anteriormente, o grupo de vantagens que um animal tem com o procedimento da castração é grande, e promove uma qualidade de vida maior para o cão e para seus donos – que ficam livres de lidar com uma série de problemas e comportamentos incômodos.<br><br>
+        Obviamente, o conjunto de benefícios varia de um cachorro para outro, e a raça do animal em questão também deve ser levada em consideração – já que cada tipo de cão têm características e personalidades específicas.<br><br>
+        Enquanto as fêmeas que passam pelo procedimento correm riscos bem menores em relação a algumas doenças; os machos castrados mostram vantagens maiores no quesito comportamental. Muitos dos problemas não são excluídos por completo da vida do animal, no entanto, é possível notar diminuições consideráveis nos hábitos mostrados a seguir:<br>
+        - Montar nas pessoas;<br>
+        - Agressividade;<br>
+        - Urinar em local inapropriado;<br>
+        - Demarcar território;<br>
+        - Desenvolvimento de tumores em testículo;<br>
+        - Aumento de próstata (Evita a HPB – hiperplasia prostática benigna).`,
+    contatoText:
+      "Para mais informações sobre esse serviço, entre em contato conosco pelo telefone: (31) 4102-0020 ou (31) 9 8334-7176",
+  },
+  {
+    id: 3,
+    title: "CIRURGIAS",
+    img: cirurgias,
+    description: "",
+    contatoText: `Para mais informações sobre esse serviço, entre em contato conosco pelo telefone: (31) 4102-0020 ou (31) 9 8334-7176`,
+  },
+  {
+    id: 4,
+    title: "LIMPEZA DE TÁRTARO",
+    img: limpezaTartaro,
+    description: `Assim como acontece com nós humanos, nossos amigos pets também precisam de uma higiene bucal para evitar complicações e doenças. O tártaro em cães é algo que normalmente não é levado a sério pelos donos, mas que a longo prazo, se não for tratado, pode causar a perda precoce dos dentes e até mesmo doenças graves e infecções nos nossos cães.<br><br>
+        O tártaro em cães e gatos nada mais é que uma placa de bactérias que surge pelo acúmulo de restos de comida nos dentes do cão e que não é retirada corretamente. O tártaro – que também é conhecido como cálculo dentário – atinge a maioria dos cães e pode ser evitado e tratado para preservar a saúde da boca de nossos cães.<br><br>
+        Se não for tratado a tempo, o tártaro pode causar, por exemplo, meningite e outras infecções generalizadas nos cães; isso porque com o avanço dessa placa bacteriana pode haver o contato com algum vaso sanguíneo, o que espalha a área de alcance da bactéria, levando a outras doenças.`,
+    contatoText: `Para mais informações sobre esse serviço, entre em contato conosco pelo telefone: (31) 4102-0020 ou (31) 9 8334-7176`,
+  },
+  {
+    id: 5,
+    title: "TAXI DOG",
+    img: taxiDog,
+    description: `Se você tem um bicho de estimação e fica preocupado toda vez que precisa levá-lo para dar banho no pet shop ou mesmo à consulta ao veterinário, saiba que já existe um meio de transporte feito especialmente para ele: o táxi dog.<br><br>
+        No táxi dog os animais são transportados em caixas individuais, fixadas dentro do carro, higienizadas e esterilizadas após cada uso para evitar a transmissão de doenças.<br><br>
+        O serviço é completamente seguro e feito exclusivamente para transportar animais com toda a segurança e higiene necessárias, para distâncias de curtas e médias distâncias.<br><br>
+        Essa ideia de transportar animais surgiu da necessidade que as pessoas tinham para levar os cães e gatos para tomar banho no pet shop, ao veterinário, para fazer exames e viagens ou transportar o corpo dos bichinhos mortos para cemitérios de animais.<br><br>
+        Como as pessoas tinham dificuldade para fazer o transporte em carros comuns ou não tinham tempo para isso. Sem falar que é proibido fazer isso em táxi comum; os animais têm que ter conforto e segurança dentro do veículo.<br><br>
+        Para realizar o transporte dos animais domésticos e os preços variam de acordo com a distância.<br><br>
+        Ultimamente os donos estão cada vez mais preocupados com o bem-estar de seus bichinhos de estimação.`,
+    contatoText: `Para mais informações sobre esse serviço, entre em contato conosco pelos telefones: (31) 4102-0020 ou (31) 3657-5714`,
+  },
+  {
+    id: 6,
+    title: "VACINAÇÂO",
+    img: vacinacao,
+    description: `A vacinação de animais é um cuidado essencial que ajuda a evitar o aparecimento de doenças graves, como a cinomose e a leptospirose, que são muito comuns em cães. Ao vacinar o animal, você está protegendo ele contra essas e outras doenças, além de evitar o sofrimento do animal e, em muitos casos, a morte precoce.<br><br>
+        Cada tipo de vacina tem o seu próprio horário para aplicação, e muitas delas são aplicadas em duas ou mais doses para garantir a eficácia completa.<br><br>
+        A vacina também precisa ser administrada regularmente para garantir que o animal continue protegido. De forma geral, a maioria das vacinas têm duração de um ano. Com isso, a imunização contra doenças como parvovirose, cinomose e leishmaniose é garantida.<br><br>
+        Vale lembrar que o reforço de vacinas deve ser feito com a maior regularidade possível, pois ele proporciona uma camada de proteção maior, em relação ao período de tempo entre uma vacinação e outra.<br><br>
+        Lembre-se que a vacinação é uma ação responsável para o bem-estar do seu pet. Caso precise de mais informações, entre em contato conosco para agendar o dia da vacinação.`,
+    contatoText: `Para mais informações sobre esse serviço, entre em contato conosco pelo telefone: (31) 4102-0020 ou (31) 9 8334-7176`,
+  },
+];
