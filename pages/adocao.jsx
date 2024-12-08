88bd6d6392844610a672aaeeede827f35a89c8da
osvaldoferreiraf <osvaldoferreira987@gmail.com>
2024-12-08 14:09:55 -0300

    removendo arquivos

--- pages/adocao.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AdocaoPage() {
-  return (
-    <div>AdocaoPage</div>
-  )
-}
--- pages/colabore.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ColaborePage() {
-  return (
-    <div>ColaborePage</div>
-  )
-}
